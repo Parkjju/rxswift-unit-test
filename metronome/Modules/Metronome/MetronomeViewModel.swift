682a1c9bb5da6338d38a47aa38e6929bc858eb12
Parkjju <rudwns3927@gmail.com>
2023-11-29 23:45:37 +0900

    feat: 뷰모델 disposeBag 추가 / 비트타입 & 아웃풋 추가 정의

--- metronome/Modules/Metronome/MetronomeViewModel.swift
+++ metronome/Modules/Metronome/MetronomeViewModel.swift
@@ -10,6 +10,15 @@ import RxCocoa
 import RxSwift
 
 class MetronomeViewModel {
+    
+    var disposeBag = DisposeBag()
+    
+    enum BeatType: String {
+        case left = "MetronomeLeft"
+        case right = "MetronomeRight"
+        case center = "MetronomeCenter"
+    }
+
     struct Input {
         
     }
@@ -17,12 +26,16 @@ class MetronomeViewModel {
     struct Output {
         let numeratorText: Driver<String>
         let denomitorText: Driver<String>
+        let beatType: Driver<BeatType>
+        let tempo: Driver<Float>
     }
     
     func transform(input: Input) -> Output {
         let numeratorText = BehaviorRelay<String>(value: "4")
         let denomitorText = BehaviorRelay<String>(value: "4")
+        let beatType = BehaviorRelay<BeatType>(value: .center)
+        let tempo = BehaviorRelay<Float>(value: 120)
         
-        return Output(numeratorText: numeratorText.asDriver(), denomitorText: denomitorText.asDriver())
+        return Output(numeratorText: numeratorText.asDriver(), denomitorText: denomitorText.asDriver(), beatType: beatType.asDriver(), tempo: tempo.asDriver())
     }
 }
