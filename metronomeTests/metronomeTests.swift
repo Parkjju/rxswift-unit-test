e0de9e0c57779d515fa2f38ee98fe02cd7815582
Parkjju <rudwns3927@gmail.com>
2023-11-29 23:46:32 +0900

    feat:  테스트코드 작성 - 템포 / 비트타입 / 시그니쳐

--- metronomeTests/metronomeTests.swift
+++ metronomeTests/metronomeTests.swift
@@ -6,11 +6,34 @@
 //
 
 import XCTest
+import RxCocoa
+import RxSwift
+import RxTest
+import RxBlocking
+
 @testable import metronome
 
 final class metronomeTests: XCTestCase {
+    var viewModel: MetronomeViewModel!
+    var input: MetronomeViewModel.Input!
+    var output: MetronomeViewModel.Output!
     
-    func 처음_시작값이_4분의4박자_인지() {
-        
+    override func setUp() {
+        viewModel = MetronomeViewModel()
+        input = MetronomeViewModel.Input()
+        output = viewModel.transform(input: input)
+    }
+    
+    func test_numerator_denomitor_시작값이_4인지() throws {
+        XCTAssertEqual(try output.denomitorText.toBlocking().first(), "4")
+        XCTAssertEqual(try output.numeratorText.toBlocking().first(), "4")
+    }
+    
+    func test_메트로놈_첫_타입이_center인지() throws {
+        XCTAssertEqual(try output.beatType.toBlocking().first(), .center)
+    }
+    
+    func test_메트로놈_첫_BPM이_120인지() throws {
+        XCTAssertEqual(try output.tempo.toBlocking().first(), 120)
     }
 }
