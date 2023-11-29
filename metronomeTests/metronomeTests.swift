472b25d86a40a595ca451c1ca487ae9933fa9fad
Parkjju <rudwns3927@gmail.com>
2023-11-30 01:31:50 +0900

    feat: 버튼 탭 옵저버블 추가 / 이벤트 정의 및 초기 인풋객제 할당 / 이벤트 커스텀

--- metronomeTests/metronomeTests.swift
+++ metronomeTests/metronomeTests.swift
@@ -17,10 +17,17 @@ final class metronomeTests: XCTestCase {
     var viewModel: MetronomeViewModel!
     var input: MetronomeViewModel.Input!
     var output: MetronomeViewModel.Output!
+    var scheduler: TestScheduler!
+    var disposeBag: DisposeBag!
     
     override func setUp() {
         viewModel = MetronomeViewModel()
-        input = MetronomeViewModel.Input()
+        scheduler = TestScheduler(initialClock: 0)
+        disposeBag = DisposeBag()
+        
+        // 1. 기본 옵저버블 생성 및 Input 객체 추가
+        // 2. 테스트 내에서 인풋 특정 요소만 조정 후 output 새롭게 transform
+        input = retrieveDefaultInputObservable()
         output = viewModel.transform(input: input)
     }
     
@@ -35,5 +42,42 @@ final class metronomeTests: XCTestCase {
     
     func test_메트로놈_첫_BPM이_120인지() throws {
         XCTAssertEqual(try output.tempo.toBlocking().first(), 120)
+    }
+    
+    func test_버튼_탭_이후_isPlaying_속성값이_변경되는지() {
+        let isPlaying = scheduler.createObserver(Bool.self)
+        
+        var testInput = retrieveDefaultInputObservable()
+        
+        testInput.controlButtonTapped = scheduler.createColdObservable([
+            .next(10, ()),
+            .next(20, ()),
+            .next(30, ())
+        ]).asObservable()
+        
+        let newOutput = viewModel.transform(input: testInput)
+        
+        newOutput.isPlaying
+            .drive(isPlaying)
+            .disposed(by: disposeBag)
+        
+        scheduler.start()
+        
+        XCTAssertEqual(isPlaying.events, [
+            .next(0, false),
+            .next(10, true),
+            .next(20, false),
+            .next(30, true)
+        ])
+    }
+    
+    override func tearDown() {
+        disposeBag = DisposeBag()
+    }
+    
+    func retrieveDefaultInputObservable() -> MetronomeViewModel.Input {
+        let controlButton = scheduler.createColdObservable([.next(10, ())])
+        
+        return MetronomeViewModel.Input(controlButtonTapped: controlButton.asObservable())
     }
 }
